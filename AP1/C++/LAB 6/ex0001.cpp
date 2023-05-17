703e9944284fb5259251bf5084225c5bd3931f6b
Amanda <amandagov42@gmail.com>
2023-05-17 13:56:25 -0300

    P

--- AP1/C++/LAB 6/ex0001.cpp
+++ AP1/C++/LAB 6/ex0001.cpp
@@ -5,8 +5,8 @@
 #include <cctype>
 using namespace std;
 
-
-int main(){
+int main()
+{
     setlocale(LC_ALL, "Portuguese");
 
     int produtos = 0, clientes;
@@ -16,21 +16,24 @@ int main(){
     cin >> clientes;
     cout << "\n\n";
 
-    for (int i = 1; i <= clientes; i++){
+    for (int i = 1; i <= clientes; i++)
+    {
         cout << "Digite o valor de cada produto: (insira um valor negativo para encerrar): " << endl;
 
-        do{
+        do
+        {
             cin >> preco;
-            
-            if (preco < 0){
+
+            if (preco < 0)
+            {
                 break;
             }
 
             totalPreco += preco;
             produtos += 1;
-        }while (preco >= 0);
+        } while (preco >= 0);
 
-        mediaProdutos = totalPreco/produtos;
+        mediaProdutos = totalPreco / produtos;
 
         cout << fixed << setprecision(2) << "A média dos produtos para esse cliente é: R$" << mediaProdutos << "\n\n\n";
 
@@ -42,6 +45,5 @@ int main(){
     mediaGasto = totalGasto / clientes;
     cout << fixed << "A média de gasto por cliente é: R$" << mediaGasto;
 
-
-return 0;
+    return 0;
 }
--- AP1/C++/LAB 6/ex0002.cpp
+++ AP1/C++/LAB 6/ex0002.cpp
@@ -5,7 +5,8 @@
 #include <cctype>
 using namespace std;
 
-int main(){
+int main()
+{
     setlocale(LC_ALL, "Portuguese");
 
     int turmas, aprovados = 0, reprovados = 0, alunos = 0;
@@ -16,36 +17,36 @@ int main(){
     cin >> turmas;
     cout << "\n\n";
 
-    for (int i = 1; i <= turmas; i++){
+    for (int i = 1; i <= turmas; i++)
+    {
         cout << "Turma " << i << endl;
 
-        while (true){
+        while (true)
+        {
             cout << "Deseja adicionar um aluno? (precione N se deseja sair): ", cin >> opcao;
-            
-           
 
-            if (toupper(opcao) == 'N'){
+            if (toupper(opcao) == 'N')
+            {
                 break;
-            } 
+            }
 
-            alunos +=1;
+            alunos += 1;
             cout << "Digite a primeira nota: ", cin >> nota1;
             cout << "Digite a segunda nota: ", cin >> nota2;
             cout << "Digite a terceira nota: ", cin >> nota3;
             cout << "\n\n";
 
-
             mediaAluno = (nota1 + nota2 + nota3) / 3;
             somaTurma += mediaAluno;
 
-            if (mediaAluno >= 7){
+            if (mediaAluno >= 7)
+            {
                 aprovados++;
             }
-            else{
+            else
+            {
                 reprovados++;
             }
-            
-            
         }
         mediaTurma = somaTurma / alunos;
         somaTurma = 0;
@@ -55,9 +56,6 @@ int main(){
 
     cout << "O número de alunos aprovados é: " << aprovados << endl;
     cout << "O número de alunos reprovados é: " << reprovados << endl;
-
-
-
 
     return 0;
 }
