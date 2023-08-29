b2c9e2121a6e79c401fdeaab6d0646996997278b
Amanda <amandagov42@gmail.com>
2023-08-28 22:11:11 -0300

    lista4

---
+++ AP2/JAVA/LISTA4/l4ex01.java
@@ -0,0 +1,21 @@
+package LISTA4;
+
+import java.util.Scanner;
+
+public class l4ex01 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite uma frase: ");
+        String frase = input.nextLine();
+
+        int tamanho = frase.length();
+
+        System.out.print("A string possui " + tamanho + " caracteres.");
+
+        System.out.print("\n\n");
+        input.close();
+
+    }
+}
